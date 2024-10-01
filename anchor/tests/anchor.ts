56f635499bb295e9cbefa38b30440bfc165397f8
itsara <khajer@gmail.com>
2024-10-02 00:27:41 +0700

    check logined with one person

--- anchor/tests/anchor.ts
+++ anchor/tests/anchor.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Anchor } from "../target/types/anchor";
+import { expect } from "chai";
+const provider = anchor.AnchorProvider.local();
 
 describe("anchor", () => {
   // Configure the client to use the local cluster.
@@ -15,15 +17,19 @@ describe("anchor", () => {
     console.log("Your transaction signature", tx);
   });
   it("should login success when I call method login", async () => {
-
     const tx = await program.methods.login().accounts({
       myAccount: myAccount.publicKey
     }).rpc();
     console.log("Your transaction signature", tx);
-
     const myAcc = await program.account.myAccount.fetch(myAccount.publicKey);
-    console.log(new Date(myAcc.lastLogin.toNumber() * 1000));
-
-
+    let lastLogin = new Date(myAcc.lastLogin.toNumber() * 1000);
+    console.log(lastLogin);
+    let d = new Date();
+    expect(d.getDay()).eq(lastLogin.getDay());
   });
+  it("check all user login when 1 person login should found 1 record", async () => {
+    let allAcount = await provider.connection.getProgramAccounts(program.programId);
+    expect(allAcount.length).equal(1);
+  });
+
 });
