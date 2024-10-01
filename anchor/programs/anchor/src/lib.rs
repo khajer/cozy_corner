a6f7e8baa62b9d9a68cbf151613fbc535cdb9214
itsara <khajer@gmail.com>
2024-10-01 23:53:34 +0700

    add test login

--- anchor/programs/anchor/src/lib.rs
+++ anchor/programs/anchor/src/lib.rs
@@ -6,11 +6,40 @@ declare_id!("3tUC61ktA8CBNyDuhmmjPPAiU8xw6en8Czz1u7WHndzj");
 pub mod anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<InitializeAccount>) -> Result<()> {
+        let account = &mut ctx.accounts.my_account;
+        account.last_login = 0;
+
+        Ok(())
+    }
+
+    pub fn login(ctx: Context<UpdateLastLogin>) -> Result<()> {
+        let account = &mut ctx.accounts.my_account;
+        account.last_login = Clock::get()?.unix_timestamp;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeAccount<'info> {
+    #[account(init, payer = user, space = 8 + std::mem::size_of::<MyAccount>())]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateLastLogin<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct MyAccount {
+    name: String,
+    level: i32,
+    character_type: i32,
+    last_login: i64, // clock.unix_timestamp, let clock = Clock::get().unwrap();
+}
